7d2d299d695192ae9eb127df95bd955ed63093a3
Vishal Kamaliya <kamaliyavishal96@gmail.com>
2020-08-01 19:31:28 +0530

    Using React componets_1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './componets/MenuComponets';
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color='info'>
-        <div className='container'>
-          <NavbarBrand href='/'>React Webapp</NavbarBrand>
-        </div>
-      </Navbar>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color='info'>
+          <div className='container'>
+            <NavbarBrand href='/'>Ritz Carlton</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
