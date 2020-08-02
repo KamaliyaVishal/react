8d52a85ea3e51dc87d9f3e3f809d66c21777267b
Vishal Kamaliya <kamaliyavishal96@gmail.com>
2020-08-02 18:03:17 +0530

    add Main componets and move logic app conatiner to Main

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './componets/MenuComponets';
-import { DISHES } from './shared/dishes';
-import Dishdetail from './componets/DishdetailComponent';
+import Main from './componets/MainComponent';
 
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      dishes: DISHES
-    };
-  }
 
   render() {
     return (
       <div className="App">
-        <Navbar dark color='info'>
-          <div className='container'>
-            <NavbarBrand href='/'>Ritz Carlton</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} />
+        <Main />
       </div>
     );
   }
