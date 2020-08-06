d239fee893c3d8dcb7d284d5af3fa0c4885ba28a
Vishal Kamaliya <kamaliyavishal96@gmail.com>
2020-08-06 09:46:12 +0530

    Fetch from Server

--- src/componets/DishdetailComponent.js
+++ src/componets/DishdetailComponent.js
@@ -3,11 +3,13 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
 import { Link } from 'react-router-dom';
 import { LocalForm, Control, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+
 
 function RenderDish({ dish }) {
   return (
     <Card>
-      <CardImg top src={dish.image} alt={dish.name} />
+      <CardImg top src={baseUrl + dish.image} alt={dish.name} />
       <CardBody>
         <CardTitle>{dish.name}</CardTitle>
         <CardText>{dish.description}</CardText>
--- src/componets/MainComponent.js
+++ src/componets/MainComponent.js
@@ -3,14 +3,14 @@ import DishDetail from './DishdetailComponent';
 import Menu from './MenuComponets';
 import Header from './HeaderComponet';
 import Footer from './FooterComponets';
-import '../App.css';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import Home from './HomeComponet';
 import Contact from './ContactComponent';
 import About from './AboutComponet';
 import { connect } from 'react-redux';
-import { addComment, fetchDishes } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
+import { addComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
+import '../App.css';
 
 
 const mapStateToProps = state => {
@@ -22,12 +22,14 @@ const mapStateToProps = state => {
     }
 }
 
-const mapDisptachToProps = dispatch => ({
-    //Thunk: dispatch the thunk
+const mapDispatchToProps = dispatch => ({
     addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
     fetchDishes: () => { dispatch(fetchDishes()) },
-    resresetFeedbackForm: () => { dispatch(actions.reset('feedback')) }
+    resetFeedbackForm: () => { dispatch(actions.reset('feedback')) },
+    fetchComments: () => dispatch(fetchComments()),
+    fetchPromos: () => dispatch(fetchPromos())
 });
+
 
 
 class Main extends Component {
@@ -42,16 +44,21 @@ class Main extends Component {
     */
     componentDidMount() {
         this.props.fetchDishes();
+        this.props.fetchComments();
+        this.props.fetchPromos();
     }
 
     render() {
 
         const HomePage = () => {
             return (
-                <Home dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+                <Home
+                    dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
                     dishesLoading={this.props.dishes.isLoading}
-                    dishesErrMess={this.props.dishes.errMess}
-                    promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+                    dishErrMess={this.props.dishes.errMess}
+                    promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
+                    promoLoading={this.props.promotions.isLoading}
+                    promoErrMess={this.props.promotions.errMess}
                     leader={this.props.leaders.filter((leader) => leader.featured)[0]}
                 />
             );
@@ -62,7 +69,8 @@ class Main extends Component {
                 <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
                     isLoading={this.props.dishes.isLoading}
                     errMess={this.props.dishes.errMess}
-                    comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+                    comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+                    commentsErrMess={this.props.comments.errMess}
                     addComment={this.props.addComment}
                 />
             );
@@ -85,4 +93,4 @@ class Main extends Component {
     }
 }
 
-export default withRouter(connect(mapStateToProps, mapDisptachToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
